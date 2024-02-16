35e670d08937658736f39bf9557099e512070630
MatiRodrigo <120686493+MatiRodrigo@users.noreply.github.com>
2024-02-16 02:35:06 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/MatiRodrigo.py
@@ -0,0 +1,36 @@
+# https://www.python.org/
+
+# Esto es un comentario
+
+""" 
+Esto es
+un comentario
+multilinea
+"""
+
+'''
+También se pueden utlilizar
+'comillas simples' para el comentario multilinea
+'''
+# Variable
+edad = 18 
+
+# Constante
+SEGUNDOS_HORA = 3600
+
+# Enteros (int)
+cantidad_huevos = 12
+
+# Flotantes (float)
+precio = 22.5
+
+# Cadenas (str)
+nombre = "Matías"
+
+# Booleanos (bool)
+esta_lloviendo = True
+es_rojo = False
+
+lenguaje = "Python"
+
+print(f"¡Hola {lenguaje}!")
